ec41bcefe75e7b6869727485c671085ba0be63d9
Stepan Illichevskii <stepan.illichevskii@maketv.rs>
2025-07-06 11:58:31 +0200

    [Wip] Add debugging

--- ip-core.el
+++ ip-core.el
@@ -11,8 +11,8 @@
   :prefix "ip-"
   :group 'applications)
 
-(defcustom ip-org-directory "~/Documents/ORG/"
-  "Directory where IP-related org files are stored."
+(defcustom ip-org-directory "~/org/ip/"
+  "Directory where IP-related Org files (e.g., company, clients, tasks) are stored."
   :type 'directory
   :group 'ip-core)
 
@@ -31,11 +31,6 @@
   :type 'string
   :group 'ip-core)
 
-(defcustom ip-upcase-properties t
-  "If non-nil, convert property names to uppercase in plists."
-  :type 'boolean
-  :group 'ip-core)
-
 (defun ip--load-org-file (filename)
   "Return parsed Org AST from FILENAME in `ip-org-directory'."
   (let ((path (expand-file-name filename ip-org-directory)))
@@ -52,52 +47,60 @@
         hl))))
 
 (defun ip--parse-properties (hl)
-  "Extract :PROPERTIES: from a headline as plist, converting numeric values."
+  "Extract :PROPERTIES: from a headline as plist."
   (let ((props (org-element-property :properties hl)))
     (if props
         (cl-loop for (k . v) in props
-                 append (list (intern (concat ":" (upcase (symbol-name k))))
-                              (if (member k '(pib maticni_broj model rate))
-                                  (string-to-number v)
-                                v)))
+                 append (list (intern (concat ":" (upcase (symbol-name k)))) v))
       '())))
 
-;;;###autoload
 (defun ip-get-clients ()
   "Return list of clients as plists from `clients.org'."
-  (let ((ast (ip--load-org-file ip-clients-file)))
-    (cl-loop for hl in (ip--get-headlines ast)
-             collect (append (list :name (org-element-property :raw-value hl))
-                             (ip--parse-properties hl)))))
+  (condition-case err
+      (let ((ast (ip--load-org-file ip-clients-file)))
+        (cl-loop for hl in (ip--get-headlines ast)
+                 collect (append (list :NAME (org-element-property :raw-value hl))
+                                 (ip--parse-properties hl))))
+    (error (message "Error loading clients: %s" err) '())))
 
-;;;###autoload
 (defun ip-get-company-info ()
   "Return company info from `company.org' as plist (first headline)."
-  (let* ((ast (ip--load-org-file ip-company-file))
-         (hl (car (ip--get-headlines ast))))
-    (append (list :name (org-element-property :raw-value hl))
-            (ip--parse-properties hl))))
+  (condition-case err
+      (let* ((ast (ip--load-org-file ip-company-file))
+             (hl (car (ip--get-headlines ast))))
+        (unless hl
+          (error "No headlines found in %s" ip-company-file))
+        (let ((props (append (list :NAME (org-element-property :raw-value hl))
+                             (ip--parse-properties hl))))
+          (unless (plist-get props :NAME)
+            (error "Company name not found in %s" ip-company-file))
+          props))
+    (error (message "Error loading company info: %s" err) '())))
 
 ;;;###autoload
 (defun ip-show-overview ()
-  "Show summary of company and client list.  Print all properties for debugging."
+  "Show summary of company and client list. Print all properties for debugging."
   (interactive)
   (let ((company (ip-get-company-info))
         (clients (ip-get-clients)))
     (with-current-buffer (get-buffer-create "*IP Overview*")
       (erase-buffer)
       (insert "Company Properties (Debug):\n")
-      (cl-loop for (key value) on company by #'cddr
-               do (insert (format "%-20s: %s\n" key value)))
+      (if company
+          (cl-loop for (key value) on company by #'cddr
+                   do (insert (format "%-20s: %s\n" key value)))
+        (insert "No company data available\n"))
       (insert "\nSummary:\n")
-      (insert (format "Company: %s\n" (plist-get company :NAME)))
-      (insert (format "Address: %s\n" (plist-get company :ADDRESS)))
-      (insert (format "IBAN: %s\n\n" (plist-get company :IBAN)))
+      (insert (format "Company: %s\n" (or (plist-get company :NAME) "N/A")))
+      (insert (format "Address: %s\n" (or (plist-get company :ADDRESS) "N/A")))
+      (insert (format "IBAN: %s\n\n" (or (plist-get company :IBAN) "N/A")))
       (insert "Clients:\n")
-      (dolist (c clients)
-        (insert (format "- %s (%s/hr)\n"
-                        (plist-get c :NAME)
-                        (or (plist-get c :RATE) "?"))))
+      (if clients
+          (dolist (c clients)
+            (insert (format "- %s (%s/hr)\n"
+                            (or (plist-get c :NAME) "Unknown")
+                            (or (plist-get c :RATE) "?"))))
+        (insert "No clients available\n"))
       (goto-char (point-min))
       (read-only-mode 1)
       (pop-to-buffer (current-buffer)))))
